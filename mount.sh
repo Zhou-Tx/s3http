611808cfa98fde122c5ccfb4a95991e51dbe454e
Zhou Tianxing <z.t.x@foxmail.com>
2024-04-14 23:49:16 +0800

    fix: permission

--- mount.sh
+++ mount.sh
@@ -4,7 +4,9 @@ if [ -z "$1" ]; then
   exit 0
 fi
 
-mkdir "/media/$1"
-s3fs "$1" "/media/$1" \
+mkdir -p "/data/$1"
+s3fs "$1" "/data/$1" \
 -o use_path_request_style,ro \
--o url=${S3_URL}
+-o url=${S3_URL} \
+-o allow_other \
+-o umask=222
